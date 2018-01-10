295ae63991c1ab570a6a705ff3203257f7357fe9
davy <davy.wybiral@gmail.com>
2018-01-10 15:31:55 -0600

    add id param

--- main.go
+++ main.go
@@ -10,14 +10,28 @@ const Timeout = time.Second
 
 func main() {
 	http.HandleFunc("/favicon.png", asset)
+	http.HandleFunc("/page1", page1)
+	http.HandleFunc("/page2", page2)
 	http.HandleFunc("/", index)
+	log.Println("Serving on :8080")
 	http.ListenAndServe(":8080", nil)
 }
 
 func asset(w http.ResponseWriter, r *http.Request) {
-	log.Print("/ ASSET IN")
-	defer log.Print("/ ASSET OUT")
+	if r.Method != "GET" {
+		return
+	}
+	t0 := time.Now()
+	query := r.URL.Query().Get("id")
+	log.Println("IN", query)
+	defer func() {
+		t1 := time.Now()
+		log.Println("OUT", query, t1.Sub(t0))
+	}()
 	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
 	flusher, ok := w.(http.Flusher)
 	if !ok {
 		return
@@ -32,13 +46,41 @@ func asset(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func page1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(`<html>
+	<head>
+		<title>Page 1</title>
+		<link rel="icon" href="/favicon.png?id=page1"/>
+	</head>
+	<body>
+		<a href="/">Index</a> - <a href="/page1">Page 1</a> - <a href="/page2">Page 2</a>
+	</body>
+</html>`))
+}
+
+func page2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(`<html>
+	<head>
+		<title>Page 2</title>
+		<link rel="icon" href="/favicon.png?id=page2"/>
+	</head>
+	<body>
+		<a href="/">Index</a> - <a href="/page1">Page 1</a> - <a href="/page2">Page 2</a>
+	</body>
+</html>`))
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(`<html>
 	<head>
-		<title>Test</title>
-		<link rel="icon" href="/favicon.png?x"/>
+		<title>Index</title>
+		<link rel="icon" href="/favicon.png?id=index"/>
 	</head>
+	<body>
+		<a href="/">Index</a> - <a href="/page1">Page 1</a> - <a href="/page2">Page 2</a>
+	</body>
 </html>`))
-	log.Print("/ PAGE LOAD")
 }
